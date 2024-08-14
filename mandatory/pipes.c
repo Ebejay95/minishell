6265ccede0f80f42405c95bc40f0f147b3c77b87
Jonathan Eberle <jeberle@student.42heilbronn.de>
2024-08-14 14:06:34 +0200

    other normings

--- mandatory/pipes.c
+++ mandatory/pipes.c
@@ -3,88 +3,77 @@
 /*                                                        :::      ::::::::   */
 /*   pipes.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chorst <chorst@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jeberle <jeberle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 11:08:56 by chorst            #+#    #+#             */
-/*   Updated: 2024/08/14 08:54:41 by chorst           ###   ########.fr       */
+/*   Updated: 2024/08/14 14:05:56 by jeberle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../include/minishell.h"
 
-static void add_seq_to_cmd_seq(t_list ***cmd_seq, t_list *seq, int *count)
+static void	add_seq_to_cmd_seq(t_list ***cmd_seq, t_list *seq, int *count)
 {
-    t_list **new_cmd_seq;
+	t_list	**new_cmd_seq;
+	int		i;
 
-    new_cmd_seq = ft_calloc(*count + 2, sizeof(t_list *));
-    if (!new_cmd_seq)
-        ft_error_exit("ft_calloc");
-
-    if (*cmd_seq)
-    {
-        for (int i = 0; i < *count; i++)
-            new_cmd_seq[i] = (*cmd_seq)[i];
-        free(*cmd_seq);
-    }
-
-    new_cmd_seq[*count] = seq;
-    (*count)++;
-    *cmd_seq = new_cmd_seq;
+	new_cmd_seq = ft_calloc(*count + 2, sizeof(t_list *));
+	if (!new_cmd_seq)
+		ft_error_exit("ft_calloc");
+	if (*cmd_seq)
+	{
+		i = 0;
+		while (i < *count)
+		{
+			new_cmd_seq[i] = (*cmd_seq)[i];
+			i++;
+		}
+		free(*cmd_seq);
+	}
+	new_cmd_seq[*count] = seq;
+	(*count)++;
+	*cmd_seq = new_cmd_seq;
 }
 
-void split_pipes(t_minishell *m, t_list ***cmd_seq, t_list ***exec_seq)
+void	split_pipes(t_minishell *m, t_list ***cmd_seq, t_list ***exec_seq)
 {
-    int count = 0;
-    t_list *current = m->tok_lst;
-    t_list *seq = NULL;
-    t_token *token;
+	int		count;
+	t_list	*current;
+	t_list	*s;
+	t_token	*token;
 
-    *cmd_seq = NULL;
-    m->pipes = 0;
-
-    while (current != NULL)
-    {
-        token = (t_token *)current->content;
-        if (token->token == PIPE)
-        {
-            if (seq != NULL)
-            {
-                add_seq_to_cmd_seq(cmd_seq, seq, &count);
-                seq = NULL;
-            }
-            m->pipes++;
-        }
-        else
-        {
-            if (seq == NULL || (token->token == COMMAND && seq->content != NULL))
-            {
-                if (seq != NULL)
-                    add_seq_to_cmd_seq(cmd_seq, seq, &count);
-                seq = NULL;
-            }
-            add_token_to_list(&seq, token);
-        }
-        current = current->next;
-    }
-
-    if (seq != NULL)
-    {
-        add_seq_to_cmd_seq(cmd_seq, seq, &count);
-    }
-
-    // Allocate exec_seq
-    *exec_seq = ft_calloc(count + 1, sizeof(t_list *));
-    if (!*exec_seq)
-        ft_error_exit("ft_calloc");
-
-    // Debug output
-    if (DEBUG == 1)
-    {
-        ft_printf("split_pipes result:\n");
-        for (int i = 0; i < count; i++)
-        {
-            ft_printf("Command sequence %d:\n", i);
-            ft_lstput(&((*cmd_seq)[i]), put_token, '\n');
-        }
-    }
+	count = 0;
+	current = m->tok_lst;
+	s = NULL;
+	*cmd_seq = NULL;
+	m->pipes = 0;
+	while (current != NULL)
+	{
+		token = (t_token *)current->content;
+		if (token->token == PIPE)
+		{
+			if (s != NULL)
+			{
+				add_seq_to_cmd_seq(cmd_seq, s, &count);
+				s = NULL;
+			}
+			m->pipes++;
+		}
+		else
+		{
+			if (s == NULL || (token->token == COMMAND && s->content != NULL))
+			{
+				if (s != NULL)
+					add_seq_to_cmd_seq(cmd_seq, s, &count);
+				s = NULL;
+			}
+			add_token_to_list(&s, token);
+		}
+		current = current->next;
+	}
+	if (s != NULL)
+		add_seq_to_cmd_seq(cmd_seq, s, &count);
+	*exec_seq = ft_calloc(count + 1, sizeof(t_list *));
+	if (!*exec_seq)
+		ft_error_exit("ft_calloc");
 }
