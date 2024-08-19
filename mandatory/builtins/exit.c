1fa770de73e427e4b4d11c2f6cbc04c9ce8d0710
chorst <chorst@student.42heilbronn.de>
2024-08-19 09:19:04 +0200

    changed exit cause it was not wokring with givin parameter

--- mandatory/builtins/exit.c
+++ mandatory/builtins/exit.c
@@ -6,22 +6,38 @@
 /*   By: chorst <chorst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 15:12:06 by chorst            #+#    #+#             */
-/*   Updated: 2024/08/13 15:30:31 by chorst           ###   ########.fr       */
+/*   Updated: 2024/08/19 09:14:17 by chorst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../../include/minishell.h"
 
+int	is_numeric(const char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[0] == '-' || str[0] == '+')
+		i++;
+	while (str[i])
+	{
+		if (!isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 void	ft_exit(char **argv, t_minishell *m)
 {
 	if (argv[1])
 	{
-		if (m->exitcode == 0 && argv[1][0] != '0')
+		if (!is_numeric(argv[1]))
 		{
-			printf("🍕🚀🌈🦄🍺: exit: %s: %s\n",
-				argv[1], NUMERIC_ARG_REQUIRED);
+			printf("🍕🚀🌈🦄🍺: exit: %s: %s\n", argv[1], NUMERIC_ARG_REQUIRED);
 			exit(2);
 		}
+		exit(ft_atoi(argv[1]));
 	}
 	cleanup_minishell(m);
 	exit(m->exitcode);
